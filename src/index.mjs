e89e2d1992e7225bfdc37a6ff275cec683a5449a
aleksandre <aleksandrekasrashvili@gmail.com>
2026-05-13 23:34:18 +0400

    changed origin in cors

--- src/index.mjs
+++ src/index.mjs
@@ -25,7 +25,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://bookmark-magaer-app.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   credentials: true
 }));
