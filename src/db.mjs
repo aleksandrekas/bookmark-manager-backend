17ef3c51f2acb26bba33ab6be36b6af9e857882c
aleksandre <aleksandrekasrashvili@gmail.com>
2026-05-17 17:28:00 +0400

    another smaa fix in connection string

--- src/db.mjs
+++ src/db.mjs
@@ -4,7 +4,7 @@ import pg, { Connection } from "pg";
 const {Pool} = pg
 
 export const database = new Pool({
-    connectionString:`//postgres:${process.env.DB_PASS}@db.qwqmhfywgttvhlkbxvhx.supabase.co:5432/postgres`,
+    connectionString: `postgresql://postgres:${process.env.DB_PASS}@db.qwqmhfywgttvhlkbxvhx.supabase.co:5432/postgres`,
     ssl:{
         rejectUnauthorized:false
     }
